2743d41de5c42dfc8456df517846807699d672b9
faahta <fasso143@gmail.com>
2019-05-27 22:55:32 +0200

    Update dot_product.c

--- dot_product.c
+++ dot_product.c
@@ -209,7 +209,7 @@ main(int argc, char *argv[]){
 	printf("Dot product as computed by me: %d\n",dot_prod);
 	printf("Dot product as computed by my niggas: %d\n",th_data.sum);
 	if(dot_prod != th_data.su)
-		printf("funny but true! I'd rather be 80 year old me than 20 year old you\n");
+		printf("my nigga u failed me, funny but true! I'd rather be 80 year old me than 20 year old you\n");
 	pthread_exit((void *) pthread_self());
 
 	return 0;
